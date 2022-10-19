c7344de42b99029c968ef81f8fdd51bd8a074e0c
Elliott Johnson <elliotthkj1991@gmail.com>
2022-10-18 23:11:32 -0700

    allowed user to answer with any case letters

--- index.js
+++ index.js
@@ -4,28 +4,27 @@ function playGame() {
 
   for (let i = 0; i < questions.length; i++) {
     let question = questions[i];
-    let userAnswer = window.prompt(question.text);
+    let userAnswer = window.prompt(question.text).toUpperCase();
     if (userAnswer === question.correctAnswer) {
       userScore = userScore + 10;
       window.alert("Your score is: " + userScore);
     }
   }
 
-  let userPlayAgainAnswer = window.prompt(
-    userName +
-      ", you finished with a score of: " +
-      userScore +
-      "!" +
-      " Play again? Yes or No."
-  );
-  if (userPlayAgainAnswer === "Yes") {
+  let userPlayAgainAnswer = window
+    .prompt(
+      userName +
+        ", you finished with a score of: " +
+        userScore +
+        "!" +
+        " Play again? Yes or no."
+    )
+    .toUpperCase();
+  if (userPlayAgainAnswer === "YES") {
     playGame();
   } else {
-          window.alert("Thanks for playing! :D");
+    window.alert("Thanks for playing! :D");
   }
 }
 
 playGame();
-
-
-// TODO: let the user answer with lowercase letters
